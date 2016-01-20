b970270cd63cbd367b570304ee4008153fcb416e
AndreasGSE <andreas.lloyd@barcelonagse.eu>
2016-01-20 21:46:00 +0100

    added some an rscript

---
+++ Rscripts/comparisongraphs.R
@@ -0,0 +1,13 @@
+trainData <- read.csv("news_popularity_training.csv")
+
+plotCompare <- function(var1,var2,data){
+  if (!require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
+  
+  ggplot(data = data, aes_string(x = var1, y = var2)) +
+    geom_point(aes(colour = as.factor(popularity))) +
+    theme_bw() +
+    scale_colour_manual("popularity",
+                        values = c("blue", "red", "black", "green", "yellow"))
+}
+
+plotCompare(var1 = "n_tokens_content", var2 = "num_imgs", data = trainData)
